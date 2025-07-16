f358ca07ebe6eded2ec7739219c84f9e3f7fe484
k7dbh <203570242+k7dbh@users.noreply.github.com>
2025-07-16 22:26:57 +0300

    fixing errors and completing files forms and routing

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const path = require('path');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 
-app.get('/',(req,res) => {
-    res.render('index.ejs')
-})
+const carsController = require('./controllers/carsController');
+
+// Middleware setup
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 //DATABASE CONNECTION
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 mongoose.connection.on('connected', () => {
-    console.log(`connected to MongoDB ${mongoose.connection.name}`)
-})
+  console.log(`connected to MongoDB ${mongoose.connection.name}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use('/cars', carsController);
 
 app.listen(3000, () => {
-    console.log('Listen on port 3000')
-})
+  console.log('Listen on port 3000');
+});
